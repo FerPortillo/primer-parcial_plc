89a19aba39c7dd764ec9e2d9b9bc33419e3dcd3b
FerPortillo <145026389+FerPortillo@users.noreply.github.com>
2023-09-15 21:39:34 -0600

    Update funcionarios.py

--- funcionarios.py
+++ funcionarios.py
@@ -1,1 +1,18 @@
-def función():
+def print():
+  print("hola hola, este es un print")
+
+print()
+
+def suma():
+  print("para sumar pides al usuario un numero")
+  print("para eso necesitas un input()")
+  suma1 = input("agrega el primer numero")
+  suma2 = input("agrega el segundo numero")
+  resultado = float(suma1 + suma2)
+  print("la suma de esos numeros es", resultado)
+  print("para multiplicar puedes usar ese numero y otro")
+  mult = input("pon otro numero")
+  multi = float(resultado*mult)
+  print("la multiplicacion sería", multi)
+
+suma()
